1587f1d0bed959218337cda1033ed71f2dcc3b08
Burke Thokchom <burke8746@gmail.com>
2024-01-24 02:12:51 +0530

    update

--- modules/api/pay/pay.controller.js
+++ modules/api/pay/pay.controller.js
@@ -7,9 +7,10 @@ exports.initiatePayment = async (req, res, next) => {
     const currencies = currenciesJSON.current;
     const reqBody = req.body;
     const resp = await paymentService.initiatePayment(reqBody);
-    console.log(resp)
+    console.log("Server resp",resp)
     return res.status(200).send({message:resp.toString()})
   } catch (error) {
+    console.log("Error in promise", error)
     next(error);
   }
 };
